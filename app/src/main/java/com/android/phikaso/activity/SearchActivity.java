2245df25c94739c05dab4bed883bd9280aed39da
Deun Lee <54697735+deunlee@users.noreply.github.com>
2022-05-23 19:06:18 +0900

    피싱 번호 조회 수정
    AsyncTask가 deprecated 되어서 제거

--- app/src/main/java/com/android/phikaso/activity/SearchActivity.java
+++ app/src/main/java/com/android/phikaso/activity/SearchActivity.java
@@ -3,31 +3,34 @@ package com.android.phikaso.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ContentValues;
 import android.graphics.Color;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.phikaso.R;
-import com.android.phikaso.RequestHttpConnection;
 import com.android.phikaso.util.StringUtil;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class SearchActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "SearchActivity";
 
     private EditText editPhoneNumber;
-    private TextView textSearchResultPolice;
-    private TextView textSearchResultMoya;
+    private TextView textSearchPolice;
+    private TextView textSearchMoya;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +40,8 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
         findViewById(R.id.search_btn_query).setOnClickListener(this);
 
         editPhoneNumber  = (EditText) findViewById(R.id.search_edit_phone);
-        textSearchResultPolice = (TextView) findViewById(R.id.search_text_result_police);
-        textSearchResultMoya = (TextView) findViewById(R.id.search_text_result_moya);
+        textSearchPolice = (TextView) findViewById(R.id.search_text_result_police);
+        textSearchMoya   = (TextView) findViewById(R.id.search_text_result_moya);
     }
 
     @Override
@@ -46,35 +49,20 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
         int id = v.getId();
         if (id == R.id.search_btn_query) {
             String num = editPhoneNumber.getText().toString();
-            searchInThePolice(num);
-            searchInTheMoya(num);
-        }
-    }
-
-    // 경찰청 피싱 번호 검색 결과
-    private void searchResultCallback(final String result) {
-        if (result == null) {
-            textSearchResultPolice.setText("조회에 실패하였습니다.");
-        } else {
-            if (result.contains("접수된 민원이 없습니다")) {
-                textSearchResultPolice.setTextColor(Color.BLACK);
-            } else {
-                textSearchResultPolice.setTextColor(Color.RED);
-            }
-            textSearchResultPolice.setText(result);
+            searchNumberPolice(num);
+            searchNumberMoya(num);
         }
     }
 
     // 경찰청 피싱 번호 검색
-    private void searchInThePolice(final String query) {
+    private void searchNumberPolice(final String query) {
         final String ts  = Long.toString(System.currentTimeMillis());
         final String cb  = "jQuery" + (ts+ts).substring(0, 21) + "_" + ts;
         final String url = "https://net-durumi.cyber.go.kr/countFraud.do?callback=" + cb + "&fieldType=H&keyword=" + query + "&accessType=3&_=" + ts;
 
         final Request.Builder builder = new Request.Builder().url(url).get();
-        builder.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36");
+        builder.addHeader("User-Agent", getString(R.string.user_agent));
         builder.addHeader("Referer", "https://cyberbureau.police.go.kr/");
-        builder.addHeader("Accept", "*/*");
         final Request request = builder.build();
 
         new Thread(() -> { // 인터넷 요청은 새 스레드에서 수행해야 함
@@ -82,63 +70,85 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
             client.newCall(request).enqueue(new Callback() {
                 @Override
                 public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                    searchResultCallback(null);
+                    searchResultPolice(null);
                 }
                 @Override
                 public void onResponse(@NonNull Call call, @NonNull final Response response) throws IOException {
-                    if (!response.isSuccessful()) {
-                        searchResultCallback(null);
+                    if (!response.isSuccessful() || response.body() == null) {
+                        searchResultPolice(null);
                         return;
                     }
                     String data = response.body().string();
                     if (!data.contains("\"success\":true")) {
-                        searchResultCallback(null);
+                        searchResultPolice(null);
                         return;
                     }
                     data = data.substring(cb.length() + "({\"message\":\"".length());
                     data = data.substring(0, data.length() - "\",\"success\":true})".length());
                     final String result = StringUtil.removeHtmlTags(StringUtil.unescapeUnicode(data));
                     runOnUiThread(() -> {
-                        searchResultCallback(result);
+                        searchResultPolice(result);
                     });
                 }
             });
         }).start();
     }
 
-    public class NetworkTask extends AsyncTask<Void, Void, String> {
-        private String url;
-        private ContentValues values;
-
-        public NetworkTask(String url, ContentValues values) {
-            this.url = url;
-            this.values = values;
-        }
-
-        @Override
-        protected String doInBackground(Void... params) {
-            String result; // 요청 결과를 저장할 변수.
-            RequestHttpConnection requestHttpURLConnection = new RequestHttpConnection();
-            result = requestHttpURLConnection.request(url, values); // 해당 URL로 부터 결과물을 얻어온다.
-            return result;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            //doInBackground()로 부터 리턴된 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
-            textSearchResultMoya.setText(s);
+    // 경찰청 피싱 번호 검색 결과
+    private void searchResultPolice(final String result) {
+        if (result == null) {
+            textSearchPolice.setText("조회에 실패하였습니다.");
+        } else {
+            if (result.contains("접수된 민원이 없습니다")) {
+                textSearchPolice.setTextColor(Color.BLACK);
+            } else {
+                textSearchPolice.setTextColor(Color.RED);
+            }
+            textSearchPolice.setText(result);
         }
     }
 
     // 뭐야이번호 피싱 번호 검색
-    private void searchInTheMoya(final String query) {
-        // URL 설정.
-        String url = "http://www.moyaweb.com/search_result.do";
-        ContentValues values = new ContentValues();
-        values.put("SCH_TEL_NO", query);
-        // AsyncTask를 통해 HttpURLConnection 수행.
-        NetworkTask networkTask = new NetworkTask(url, values);
-        networkTask.execute();
+    private void searchNumberMoya(final String query) {
+        final String      url  = "http://www.moyaweb.com/search_result.do";
+        final RequestBody body = new FormBody.Builder().add("SCH_TEL_NO", query).build();
+
+        final Request.Builder builder = new Request.Builder().url(url).post(body);
+        builder.addHeader("User-Agent", getString(R.string.user_agent));
+        builder.addHeader("Referer", "http://www.moyaweb.com/main.do");
+        final Request request = builder.build();
+
+        new Thread(() -> {
+            final OkHttpClient client = new OkHttpClient();
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                    searchResultMoya(null);
+                }
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull final Response response) throws IOException {
+                    if (!response.isSuccessful() || response.body() == null) {
+                        searchResultMoya(null);
+                        return;
+                    }
+                    String data = response.body().string();
+                    Document doc = Jsoup.parse(data);
+                    Elements content_td = doc.getElementsByClass("content_td");
+                    String result = content_td.text();
+                    runOnUiThread(() -> {
+                        searchResultMoya(result);
+                    });
+                }
+            });
+        }).start();
+    }
+
+    private void searchResultMoya(final String result) {
+        if (result == null) {
+            textSearchMoya.setText("조회에 실패하였습니다.");
+        } else {
+//            textSearchPolice.setTextColor(Color.RED);
+            textSearchMoya.setText(result);
+        }
     }
 }
